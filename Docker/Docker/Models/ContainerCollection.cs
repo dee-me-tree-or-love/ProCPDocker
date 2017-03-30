5bc3f99b538ea359eddbdde063381199fb66a6be
Dmitry <dmitry-orlov@hotmail.com>
2017-03-30 10:31:34 +0200

    Tiny models: issue#30 added the models for the ship, container collection, dock and container

---
+++ Docker/Docker/Models/ContainerCollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Docker.Models
+{
+    public class ContainerCollection
+    {
+        public int ID { get; set; }
+
+        // The name property that came from the Storage class in the CD.
+        public String Name { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        // If you specify ICollection<T>, EF creates a HashSet<T> collection by default.
+        // the conteents of the collection
+        public ICollection<Container> Containers { get; set; }
+    }
+}
---
+++ Docker/Docker/Models/Ship.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Docker.Models
+{
+    // inherits from the container collection model
+    public class Ship : ContainerCollection
+    {
+        // ID is inhertited from the CC
+
+        public String Tag { get; set; }
+        // If you specify ICollection<T>, EF creates a HashSet<T> collection by default.
+        // Container requirement lists
+        public List<String> ContainerIDsLoad { get; set; }
+        public List<String> ContainerIDsUnload { get; set; }
+
+        // Plus would require ETA and other properties which are out of scope of this iteration
+    }
+}
