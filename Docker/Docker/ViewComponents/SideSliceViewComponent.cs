bf8224bb2bbe771b4f3be989ae49ddd8247ec50b
i333656 <v.stoyanov@student.fontys.nl>
2017-04-02 12:27:01 +0200

    add both side and front ViewComponent of the container clusters

--- Docker/Docker/ViewComponents/SideSliceViewComponent.cs
+++ Docker/Docker/ViewComponents/SideSliceViewComponent.cs
@@ -7,21 +7,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Docker.ViewComponents
 {
-    public class FrontSliceViewComponent : ViewComponent
+    public class SideSliceViewComponent : ViewComponent
     {
         public async Task<IViewComponentResult> InvokeAsync(ContainerCollection cc)
         {
-            int y = 0;
-            Container[,] slice = new Container[cc.X, cc.Z];
+            int x = 0;
+            Container[,] slice = new Container[cc.Y, cc.Z];
 
             foreach (Container ccContainer in cc.Containers)
             {
                 int tempX = ccContainer.X;
                 int tempY = ccContainer.Y;
                 int tempZ = ccContainer.Z;
-                if (tempY == y)
+                if (tempX == x)
                 {
-                    slice[tempX, tempZ] = ccContainer;
+                    slice[tempY, tempZ] = ccContainer;
                 }
             }
 
