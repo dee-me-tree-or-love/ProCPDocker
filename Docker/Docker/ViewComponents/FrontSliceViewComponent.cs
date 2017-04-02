742985e16d6a3a9127022a62fe851eaae4cfbf80
i333656 <v.stoyanov@student.fontys.nl>
2017-04-02 11:29:41 +0200

    add ViewComponent for displaying the containers

---
+++ Docker/Docker/ViewComponents/FrontSliceViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Docker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Docker.ViewComponents
+{
+    public class FrontSliceViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(ContainerCollection cc)
+        {
+            int x = 2;
+            Container[,] slice = new Container[cc.X, cc.Z];
+
+            foreach (Container ccContainer in cc.Containers)
+            {
+                int tempX = ccContainer.X;
+                int tempZ = ccContainer.Z;
+                if (tempX == x)
+                {
+                    slice[tempX, tempZ] = ccContainer;
+                }
+            }
+
+            return View(slice);
+        }
+    }
+}
