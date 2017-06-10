9c383caa8dbd55e744d620e775bb07adcce11cd7
Dmitry <dmitry-orlov@hotmail.com>
2017-06-10 14:40:45 +0200

    merged the changes

--- proto/Backend/Shared/ContainerLoader/index.js
+++ proto/Backend/Shared/ContainerLoader/index.js
@@ -36,7 +36,7 @@ module.exports.linearLoad = (container, container_hold) => {
             z = counter % container_hold.z;
         }
     }
-    return {x: x, y: y, z: z};
+    return { x: x, y: y, z: z };
 };
 
 
@@ -70,11 +70,11 @@ module.exports.weightBasedLoad = (container, container_hold) => {
 
             // no containers on this X axis have been encountered => no y containers of this x axis are also 
             cWieghtMatrix[cellCoordX] = {}
-            cWieghtMatrix[cellCoordX][cellCoordY] = {weight: container_hold.containers_in[key].weight, count: 1};
+            cWieghtMatrix[cellCoordX][cellCoordY] = { weight: container_hold.containers_in[key].weight, count: 1 };
         } else if (!cWieghtMatrix[cellCoordX][cellCoordY]) {
 
             // there have been containers on X axis, but not with this Y coordinate
-            cWieghtMatrix[cellCoordX][cellCoordY] = {weight: container_hold.containers_in[key].weight, count: 1};
+            cWieghtMatrix[cellCoordX][cellCoordY] = { weight: container_hold.containers_in[key].weight, count: 1 };
         } else {
 
             // there have already been recorded the containers on this X and Y => this container is stacked on top of it (or beneath) and should be added to the column mass weight
@@ -106,20 +106,20 @@ module.exports.weightBasedLoad = (container, container_hold) => {
 
         iterationNumber++;
         let makeNewCut = () => {
-            return {
-                start_w: '',
-                start_l: '',
-                end_w: '',
-                end_l: '',
-                getWidth: function () {
-                    return this.end_w - this.start_w
-                },
-                getLength: function () {
-                    return this.end_l - this.start_l
+                return {
+                    start_w: '',
+                    start_l: '',
+                    end_w: '',
+                    end_l: '',
+                    getWidth: function() {
+                        return this.end_w - this.start_w
+                    },
+                    getLength: function() {
+                        return this.end_l - this.start_l
+                    }
                 }
             }
-        }
-        // console.log(`is width parallel: ${isParallelToWidth}; iteration number: ${iterationNumber}`)
+            // console.log(`is width parallel: ${isParallelToWidth}; iteration number: ${iterationNumber}`)
         let cuts = [makeNewCut(), makeNewCut()]
 
         if (isParallelToWidth) {
@@ -174,9 +174,9 @@ module.exports.weightBasedLoad = (container, container_hold) => {
             }
         }
         let eq = ((cuts[0].end_l == cuts[0].end_l) &&
-        ((cuts[0].start_l == cuts[0].start_l) &&
-        (cuts[0].end_w == cuts[0].end_w) &&
-        (cuts[0].start_w == cuts[0].start_w)));
+            ((cuts[0].start_l == cuts[0].start_l) &&
+                (cuts[0].end_w == cuts[0].end_w) &&
+                (cuts[0].start_w == cuts[0].start_w)));
         if ((bestCut.getLength() * bestCut.getWidth() == 1) || eq) {
             // console.log("best cut: ")
             // console.log(bestCut);
@@ -193,13 +193,18 @@ module.exports.weightBasedLoad = (container, container_hold) => {
     let bestY = bestPosition.start_l;
     let bestZ = height_level;
 
-    return {x: bestX, y: bestY, z: bestZ}
+    return { x: bestX, y: bestY, z: bestZ }
     // return { x: 0, y: 0, z: 0 }
 };
 
 const validate = require('jsonschema').validate;
 module.exports.ShipLoader = class ShipLoader {
 
+    /**
+     * 
+     * @param {Object} ship
+     * @param {Array} ship.containers_in 
+     */
     constructor(ship) {
 
         this.ship = Object.assign({}, ship);
@@ -220,7 +225,7 @@ module.exports.ShipLoader = class ShipLoader {
         }
         return this.ship.containers_in.map(container => {
 
-            if(typeof container.weight === 'undefined'){
+            if (typeof container.weight === 'undefined') {
 
                 return 0;
             }
@@ -231,9 +236,50 @@ module.exports.ShipLoader = class ShipLoader {
         }, 0);
     }
 
-    getDistanceBetweenTwoPoints(){
+    getDistanceBetweenTwoPoints() {
 
     }
+
+    getPlacementPossibilities() {
+        let options = [];
+        let zMappingMatrix = {};
+
+        // get the list of all the options in the bottom level
+        for (let xPos = 0; xPos < this.ship.x; xPos++) {
+
+            // check the mapping option
+            if (!zMappingMatrix[xPos]) {
+
+                zMappingMatrix[xPos] = {};
+            }
+
+            for (let yPos = 0; yPos < this.ship.y; yPos++) {
+
+                if (!zMappingMatrix[xPos][yPos]) {
+
+                    zMappingMatrix[xPos][yPos] = {};
+                }
+
+                let option = { x: xPos, y: yPos, z: 0 };
+                zMappingMatrix[xPos][yPos].option = option;
+
+                options.push(option);
+            }
+        }
+
+        for (let key in this.ship.containers_in) {
+            let xPos = this.ship.containers_in[key].x;
+            let yPos = this.ship.containers_in[key].y;
+            let zPos = this.ship.containers_in[key].z;
+            // if the z entry of the options is less or equal to the discovered one, update to +1
+            if (zMappingMatrix[xPos][yPos].option.z <= zPos) {
+                zMappingMatrix[xPos][yPos].option.z = zPos + 1;
+            }
+        }
+        this.allPossibilities = options;
+        return this.allPossibilities;
+    }
+
 };
 
 
--- proto/Backend/Shared/ContainerLoader/test.js
+++ proto/Backend/Shared/ContainerLoader/test.js
@@ -1,95 +1,233 @@
 const expect = require('chai').expect;
 const ShipLoader = require('./').ShipLoader;
-describe('Ship Loading Algorithm', function () {
+describe('Ship Loading Algorithm', function() {
 
-    describe('Calculate total mass for all containers on ship', function () {
+            describe('Calculate total mass for all containers on ship', function() {
 
-        let ship = {
-            x: 10,
-            y: 10,
-            z: 20,
-            containers_in: [
-                {
-                    "address": {
-                        "location_id": "",
-                        "x": 0,
-                        "y": 0,
-                        "z": 0
-                    },
-                    "weight": 20
-                },
-                {
-                    "address": {
-                        "location_id": "",
-                        "x": 0,
-                        "y": 0,
-                        "z": 0
-                    },
-                    "weight": 30
-                },
-                {
-                    "address": {
-                        "location_id": "",
-                        "x": 0,
-                        "y": 0,
-                        "z": 0
-                    },
-                    "weight": 50
-                }
-            ]
-        };
+                let ship = {
+                    x: 10,
+                    y: 10,
+                    z: 20,
+                    containers_in: [{
+                            "address": {
+                                "location_id": "",
+                                "x": 0,
+                                "y": 0,
+                                "z": 0
+                            },
+                            "weight": 20
+                        },
+                        {
+                            "address": {
+                                "location_id": "",
+                                "x": 0,
+                                "y": 0,
+                                "z": 0
+                            },
+                            "weight": 30
+                        },
+                        {
+                            "address": {
+                                "location_id": "",
+                                "x": 0,
+                                "y": 0,
+                                "z": 0
+                            },
+                            "weight": 50
+                        }
+                    ]
+                };
 
-        it('up getTotalMass', function () {
+                it('up getTotalMass', function() {
 
-            let sl = new ShipLoader(ship);
-            expect(sl).to.respondsTo('getTotalMass');
-        });
+                    let sl = new ShipLoader(ship);
+                    expect(sl).to.respondsTo('getTotalMass');
+                });
 
-        it('create a proper object', function () {
+                it('create a proper object', function() {
 
-            let sl = new ShipLoader(ship);
-            expect(sl.center.x).to.equal(5);
-            expect(sl.center.y).to.equal(5);
-            expect(sl.center.z).to.equal(0);
+                    let sl = new ShipLoader(ship);
+                    expect(sl.center.x).to.equal(5);
+                    expect(sl.center.y).to.equal(5);
+                    expect(sl.center.z).to.equal(0);
 
-        });
+                });
 
-        it('should give the correct total mass for a filled ship', function () {
+                it('should give the correct total mass for a filled ship', function() {
 
-            let sl = new ShipLoader(ship);
-            let totalMass = sl.getTotalMass();
-            expect(totalMass).to.equal(100);
-        });
+                    let sl = new ShipLoader(ship);
+                    let totalMass = sl.getTotalMass();
+                    expect(totalMass).to.equal(100);
+                });
 
-        it('should return 0 if there are no containers on the ship', function () {
+                it('should return 0 if there are no containers on the ship', function() {
 
-            let shipWithoutcontainers = Object.assign({}, ship);
-            delete shipWithoutcontainers.containers_in;
-            let sl = new ShipLoader(shipWithoutcontainers);
-            let totalMass = sl.getTotalMass();
-            expect(totalMass).to.equal(0);
-        });
+                    let shipWithoutcontainers = Object.assign({}, ship);
+                    delete shipWithoutcontainers.containers_in;
+                    let sl = new ShipLoader(shipWithoutcontainers);
+                    let totalMass = sl.getTotalMass();
+                    expect(totalMass).to.equal(0);
+                });
 
-        it('should skip container if it doesn\'t have weight property', function () {
+                it('should skip container if it doesn\'t have weight property', function() {
 
-            let shipWithWeightlessContainers = Object.assign({}, ship);
-            delete shipWithWeightlessContainers.containers_in[0].weight;
-            let sl = new ShipLoader(ship);
-            let totalMass = sl.getTotalMass();
-            expect(totalMass).to.equal(80);
-        });
-    });
+                    let shipWithWeightlessContainers = Object.assign({}, ship);
+                    delete shipWithWeightlessContainers.containers_in[0].weight;
+                    let sl = new ShipLoader(ship);
+                    let totalMass = sl.getTotalMass();
+                    expect(totalMass).to.equal(80);
+                });
+            });
 
-    describe('Get distance between 2 points in 3D space', function () {
+            describe('Get distance between 2 points in 3D space', function() {
 
-        const ship = {
-            x: 10, y: 10, z: 10
-        };
-        it('up getDistanceBetweenTwoPoints', function () {
+                        const ship = {
+                            x: 10,
+                            y: 10,
+                            z: 10
+                        };
+                        it('up getDistanceBetweenTwoPoints', function() {
 
-            let sl = new ShipLoader(ship);
-            expect(sl).to.respondsTo('getDistanceBetweenTwoPoints');
-        });
-        it('');
-    });
-});
+                            let sl = new ShipLoader(ship);
+                            expect(sl).to.respondsTo('getDistanceBetweenTwoPoints');
+                        });
+                        it('');
+                        describe('Ship Loading Algorithm', function() {
+
+                            describe('Calculate total mass for all containers on ship', function() {
+
+                            });
+                            describe('Get a list of all the placement possibilities', () => {
+                                it("up getPlacementPossibilities()", () => {
+                                    let Ship = {
+                                        x: 4,
+                                        y: 4,
+                                        z: 4,
+                                        containers_in: []
+                                    };
+                                    let SL = new ShipLoader(Ship);
+                                    expect(SL).to.respondsTo("getPlacementPossibilities");
+                                });
+                                it("returns (0,0,0) to (3,3,0) ofr an empty 4x4x4 container hold", () => {
+                                    let Ship = {
+                                        x: 4,
+                                        y: 4,
+                                        z: 4,
+                                        containers_in: []
+                                    };
+                                    let SL = new ShipLoader(Ship);
+                                    let optionsResult = SL.getPlacementPossibilities();
+
+                                    let expectedOptions = [];
+                                    for (let xPos = 0; xPos < 4; xPos++) {
+                                        for (let yPos = 0; yPos < 4; yPos++) {
+                                            expectedOptions.push({ x: xPos, y: yPos, z: 0 });
+                                        }
+                                    }
+
+                                    expect(optionsResult.length).to.be.equal(16);
+                                    expect(optionsResult).to.be.deep.equal(expectedOptions);
+                                });
+                                it("in a storage hold with 1 container at (1,1,0) returns the list of (0,0,0) to (3,3,0) with (1,1,1)", () => {
+
+                                    let Ship = {
+                                        x: 4,
+                                        y: 4,
+                                        z: 4,
+                                        containers_in: [{ x: 1, y: 1, z: 0 }]
+                                    };
+                                    let SL = new ShipLoader(Ship);
+                                    let optionsResult = SL.getPlacementPossibilities();
+
+                                    let expectedOptions = [];
+                                    for (let xPos = 0; xPos < 4; xPos++) {
+                                        for (let yPos = 0; yPos < 4; yPos++) {
+                                            let zPos = 0;
+                                            if (xPos == 1 && yPos == 1) {
+                                                zPos = 1;
+                                            }
+                                            expectedOptions.push({ x: xPos, y: yPos, z: zPos });
+                                        }
+                                    }
+
+                                    expect(optionsResult.length).to.be.equal(16);
+                                    expect(optionsResult).to.be.deep.equal(expectedOptions);
+                                });
+
+                                it("in a storage hold with 3 container at (1,1,0); (1,0,0); (3,0,0)" +
+                                    " returns the list of (0,0,0) to (3,3,0) " +
+                                    "with (1,1,1);(1,0,1);(3,0,1)", () => {
+
+                                        let Ship = {
+                                            x: 4,
+                                            y: 4,
+                                            z: 4,
+                                            containers_in: [
+                                                { x: 1, y: 1, z: 0 },
+                                                { x: 1, y: 0, z: 0 },
+                                                { x: 3, y: 0, z: 0 }
+                                            ]
+                                        };
+                                        let SL = new ShipLoader(Ship);
+                                        let optionsResult = SL.getPlacementPossibilities();
+
+                                        let expectedOptions = [];
+                                        for (let xPos = 0; xPos < 4; xPos++) {
+                                            for (let yPos = 0; yPos < 4; yPos++) {
+                                                let zPos = 0;
+                                                if ((xPos == 1 && yPos == 1) ||
+                                                    (xPos == 1 && yPos == 0) ||
+                                                    (xPos == 3 && yPos == 0)) {
+                                                    zPos = 1;
+                                                }
+                                                expectedOptions.push({ x: xPos, y: yPos, z: zPos });
+                                            }
+                                        }
+
+                                        expect(optionsResult.length).to.be.equal(16);
+                                        expect(optionsResult).to.be.deep.equal(expectedOptions);
+                                        expect(SL.allPossibilities).to.be.deep.equal(expectedOptions);
+                                    });
+
+
+                                it("in a storage hold with 4 containers at (1,1,0); (1,0,0); (3,0,0); (3,0,1)" +
+                                    " returns the list of (0,0,0) to (3,3,0) " +
+                                    "with (1,1,1);(1,0,1);(3,0,2)", () => {
+
+                                        let Ship = {
+                                            x: 4,
+                                            y: 4,
+                                            z: 4,
+                                            containers_in: [
+                                                { x: 1, y: 1, z: 0 },
+                                                { x: 1, y: 0, z: 0 },
+                                                { x: 3, y: 0, z: 0 },
+                                                { x: 3, y: 0, z: 1 }
+                                            ]
+                                        };
+                                        let SL = new ShipLoader(Ship);
+                                        let optionsResult = SL.getPlacementPossibilities();
+
+                                        let expectedOptions = [];
+                                        for (let xPos = 0; xPos < 4; xPos++) {
+                                            for (let yPos = 0; yPos < 4; yPos++) {
+                                                let zPos = 0;
+                                                if ((xPos == 1 && yPos == 1) ||
+                                                    (xPos == 1 && yPos == 0)) {
+                                                    zPos = 1;
+                                                }
+                                                if ((xPos == 3 && yPos == 0)) {
+                                                    zPos = 2;
+                                                }
+                                                expectedOptions.push({ x: xPos, y: yPos, z: zPos });
+                                            }
+                                        }
+
+                                        expect(optionsResult.length).to.be.equal(16);
+                                        expect(optionsResult).to.be.deep.equal(expectedOptions);
+                                    });
+                            });
+                            describe('Get Center Of Mass', () => {
+
+                            })
+                        });
